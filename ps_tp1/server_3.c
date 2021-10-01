d8d708d75bbaf73a20ed701eac9e50e1e4d21dfa
Gromauguste <76079520+Gromauguste@users.noreply.github.com>
2021-10-01 16:24:07 +0200

    2.1 wait for child process

--- ps_tp1/server_3.c
+++ ps_tp1/server_3.c
@@ -7,6 +7,16 @@
 
 //On peut distinguer les messages du père et du fils par leur PID, les deux processus s'arrêtent avec CTRL+C
 
+//A l'aide de la commande kill sur le processus fils, le processus père continue de s'exécuter mais le processus fils n'est pas
+//toujours visible ("[server_3] <defunct>"), lorsque l'on kill le père, les deux processus disparaissent
+
+//Lorsque l'on tue le père en premier, le processus père disparaît et le processus fils continue de s'exécuter, on peut toujours
+//kill le processus fils
+
+//A l'aide de wait, on peut lancer le processus père lorsque le processus fils est kill.
+//Le code renvoyé est 0 pour un kill ou un kill -s INT, et 9 pour un kill -9
+
+
 // for printf()
 #include <stdio.h>
 // For rand(), srand(), sleep(), EXIT_SUCCESS
@@ -15,8 +25,12 @@
 #include <time.h>
 // For getpid(), getppid()
 #include <unistd.h>
+
 #include <signal.h>
 #include <stdbool.h>
+
+// Wait for process
+#include <sys/wait.h>
 
 bool running = true;
 
@@ -26,16 +40,23 @@ void stop_handler( int sig ) {
 }
 
 void exit_message() {
-    printf("Allez bisous!\n");
+    printf("\nAllez bisous!\n");
 }
 
 int main()
 {   
+    //fork pid with error handling
     int cpid = fork();
     if (cpid == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
+
+    //Wait for child process to be ended before killing
+    int waitstatus;
+    wait(&waitstatus);
+
+    printf("\nwaitstatus: %d\n", waitstatus);
 
     int pid;
     int ppid;
@@ -49,16 +70,16 @@ int main()
     while (running) {
         //handling CTRL+C
         sigaction(SIGINT, &action, NULL);
+
         //handling 'kill' command
         sigaction(SIGTERM, &action, NULL);
-
 
         pid = getpid();
         ppid = getppid();
         rand_nb = rand() % 99 + 1;
 
-        printf("Current pid: %d \r\nParent pid: %d \r\n", pid, ppid) ;
-        printf("Random number: %d \r\n", rand_nb);
+        printf("\nCurrent pid: %d\nParent pid: %d\n", pid, ppid) ;
+        printf("Random number: %d\n", rand_nb);
 
         sleep(1);
     }
