8451cc80f809b903317e2c449a8be066173e930b
Jérôme AUGUSTE <jerome.auguste@student-cs.fr>
2021-10-01 15:04:49 +0200

    1.2 SIGINT handling

--- ps_tp1/server_2.c
+++ ps_tp1/server_2.c
@@ -5,3 +5,53 @@
  * server_2.c
  */
 
+
+// for printf()
+#include <stdio.h>
+// For rand(), srand(), sleep(), EXIT_SUCCESS
+#include <stdlib.h>
+// For time()
+#include <time.h>
+// For getpid(), getppid()
+#include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
+
+bool running = true;
+
+void stop_handler( int sig ) {
+    printf("\nRecieved signal: %d \n", sig);
+    running = false;
+}
+
+int main()
+{   
+    // TODO
+    int pid;
+    int ppid;
+    int rand_nb;
+
+    struct sigaction action;
+    action.sa_handler = &stop_handler;
+    action.sa_flags=0;
+
+    //Lorsque le processus est kill depuis un autre terminal avec l'option -s INT, le message apparaît, toutefois, lorsqu'il
+    //n'y a pas les options, le message n'apparaît pas
+    //Handle SIGTERM permet d'afficher le message lorsque le processus est kill sans argument
+
+    while (running) {
+        sigaction(SIGINT, &action, NULL);
+        sigaction(SIGTERM, &action, NULL);
+        pid = getpid();
+        ppid = getppid();
+        rand_nb = rand() % 99 + 1;
+
+        printf("Current pid: %d \r\nParent pid: %d \r\n", (pid, ppid)) ;
+        printf("Random number: %d \r\n", rand_nb);
+
+        sleep(1);
+    }
+    
+
+    return EXIT_SUCCESS;
+}
