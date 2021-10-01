7f091c273816b809ea0f72cbcf5d85669a369f4f
Jérôme AUGUSTE <jerome.auguste@student-cs.fr>
2021-10-01 14:27:58 +0200

    1.1

--- ps_tp1/server_1.c
+++ ps_tp1/server_1.c
@@ -17,7 +17,21 @@
 int main()
 {
     // TODO
-        
+    int pid;
+    int ppid;
+    int rand_nb;
+    while (1) {
+        pid = getpid();
+        ppid = getppid();
+        rand_nb = rand() % 99 + 1;
+
+        printf("Current pid: %d \r\nParent pid: %d \r\n", (pid, ppid)) ;
+        printf("Random number: %d \r\n", rand_nb);
+
+        sleep(1);
+    }
+    
+
     return EXIT_SUCCESS;
 }
 
