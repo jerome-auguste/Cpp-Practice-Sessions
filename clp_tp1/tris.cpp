c638e9424f37079331a90aa8ac61026f7d70139c
Gromauguste <76079520+Gromauguste@users.noreply.github.com>
2021-10-05 15:45:41 +0200

    1.5 Tri avec fonction lambda

--- clp_tp1/tris.cpp
+++ clp_tp1/tris.cpp
@@ -57,6 +57,11 @@ void sort_tab_2(std::vector< int > &tab, bool (*comp)(int, int)) {
     std::sort(tab.begin(), tab.end(), comp);
 }
 
+void sort_tab_3(std::vector< int > &tab, std::function< bool(int, int) > comp) {
+    std::sort(tab.begin(), tab.end(), comp);
+}
+
+
 void test_11() {
     std::cout << std::endl << "*** test_11 ***" << std::endl;
     const std::vector< int > tab{ 1, -2, 3, -4, 5, -6 };
@@ -74,29 +79,60 @@ void test_12() {
 
 void test_13() {
     std::cout << std::endl << "*** test_13 ***" << std::endl;
+
     std::vector< int > tab (10);
     random_tab(tab);
     std::cout << std::endl << "Random tab" << std::endl;
     print_tab(tab);
+
     sort_tab_1(tab);
     std::cout << std::endl << "Sorted tab" << std::endl;
     print_tab(tab);
+
     std::cout << std::endl;
 }
 
 void test_14() {
     std::cout << std::endl << "*** test_14 ***" << std::endl;
+
     std::vector< int > tab_greater (10);
     random_tab(tab_greater);
+
     std::vector< int > tab_less = tab_greater;
+
     std::cout << std::endl << "Random tab" << std::endl;
     print_tab(tab_greater);
+
     sort_tab_2(tab_greater, greater);
     std::cout << std::endl << "Sorted tab with greater" << std::endl;
     print_tab(tab_greater);
+
     sort_tab_2(tab_less, less);
     std::cout << std::endl << "Sorted tab with less" << std::endl;
     print_tab(tab_less);
+
+    std::cout << std::endl;
+}
+
+void test_15() {
+    std::cout << std::endl << "*** test_15 ***" << std::endl;
+
+    std::vector< int > tab_asc (10);
+    random_tab(tab_asc);
+
+    std::vector< int > tab_desc = tab_asc;
+
+    std::cout << std::endl << "Random tab" << std::endl;
+    print_tab(tab_asc);
+
+    sort_tab_3(tab_asc, []( int a, int b ) { return a<b; });
+    std::cout << std::endl << "Sorted tab in ascending order" << std::endl;
+    print_tab(tab_asc);
+
+    sort_tab_3(tab_desc, []( int a, int b ) { return a>b; });
+    std::cout << std::endl << "Sorted tab in descending order" << std::endl;
+    print_tab(tab_desc);
+
     std::cout << std::endl;
 }
 
@@ -107,7 +143,8 @@ int main()
     //test_11();
     //test_12();
     //test_13();
-    test_14();
+    //test_14();
+    test_15();
 
     return 0;
 }
