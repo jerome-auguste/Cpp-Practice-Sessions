f518a42de6e65c54d24d48630b76fe92e13e26d3
Gromauguste <76079520+Gromauguste@users.noreply.github.com>
2021-10-05 16:19:44 +0200

    2.2 Application

--- clp_tp1/listes.cpp
+++ clp_tp1/listes.cpp
@@ -21,15 +21,23 @@ std::forward_list< int > random_list( int length ) {
     return list;
 }
 
-void print_list( const std::forward_list< int > list ) {
+void print_list( const std::forward_list< int > &list ) {
     std::cout << "( ";
     for( auto el : list ) std::cout << el << " ";
     std::cout << ")" << std::endl;
 }
 
+std::forward_list< int > map_iter( const std::forward_list< int > &list, std::function< int( int ) > fct ) {
+    std::forward_list< int > modified_list;
+    for( auto el : list ) {
+        modified_list.push_front(fct(el));
+    }
+    return modified_list;
+}
+
 void test_21() {
-    std::cout << std::endl << "*** test_15 ***" << std::endl;
-    
+    std::cout << std::endl << "*** test_21 ***" << std::endl;
+
     const std::forward_list< int > list = random_list(10);
     print_list(list);
 
@@ -37,10 +45,27 @@ void test_21() {
 
 }
 
+void test_22() {
+    std::cout << std::endl << "*** test_22 ***" << std::endl;
+
+    const std::forward_list< int > list = random_list(10);
+    std::cout << std::endl << "Random list" << std::endl;
+    print_list(list);
+
+    std::forward_list< int > three_times_list = map_iter(list, []( int a ){ return 3*a; });
+    std::cout << std::endl << "3* list" << std::endl;
+    print_list(three_times_list);
+
+    std::cout << std::endl;
+}
+
 int main()
 {
     std::srand( std::time( nullptr ));
-    test_21();
+
+    //test_21();
+    test_22();
+
     return 0;
 }
 
