2a7734711e72e5afd5f8e957f93d69b44bf4c50a
Gromauguste <76079520+Gromauguste@users.noreply.github.com>
2021-10-05 14:48:40 +0200

    Changement de script pour partie 1

--- clp_tp1/listes.cpp
+++ clp_tp1/listes.cpp
@@ -12,29 +12,11 @@
 
 #include <cstdlib>
 #include <ctime>
-#include <vector>
 
-
-void print_tab(const std::vector< int > tab) {
-    std::cout << "[ ";
-    for (int i=0; i<tab.size(); i++) {
-        std::cout << tab[i] << " ";
-    }
-    std::cout << "]" << std::endl;
-}
-
-void test11() {
-    std::cout << std::endl << "*** test_11 ***" << std::endl;
-    const std::vector< int > tab{ 1, -2, 3, -4, 5, -6 };
-    print_tab(tab);
-    std::cout << std::endl;
-}
 
 int main()
 {
     std::srand( std::time( nullptr ));
-
-    test11();
 
     return 0;
 }
