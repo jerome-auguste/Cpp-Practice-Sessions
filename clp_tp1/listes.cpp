dff4f55e77194c37dadcd1b7472282083f398a2f
Gromauguste <76079520+Gromauguste@users.noreply.github.com>
2021-10-05 16:02:20 +0200

    2.1 Création, affichage

--- clp_tp1/listes.cpp
+++ clp_tp1/listes.cpp
@@ -13,11 +13,34 @@
 #include <cstdlib>
 #include <ctime>
 
+std::forward_list< int > random_list( int length ) {
+    std::forward_list< int > list;
+    for( int i=0; i<length; i++ ) {
+        list.push_front( rand() % 100 );
+    }
+    return list;
+}
+
+void print_list( const std::forward_list< int > list ) {
+    std::cout << "( ";
+    for( auto el : list ) std::cout << el << " ";
+    std::cout << ")" << std::endl;
+}
+
+void test_21() {
+    std::cout << std::endl << "*** test_15 ***" << std::endl;
+    
+    const std::forward_list< int > list = random_list(10);
+    print_list(list);
+
+    std::cout << std::endl;
+
+}
 
 int main()
 {
     std::srand( std::time( nullptr ));
-
+    test_21();
     return 0;
 }
 
