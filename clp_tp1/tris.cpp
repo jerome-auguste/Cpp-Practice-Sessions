3799a030651227690d9deb883467041b25929676
Gromauguste <76079520+Gromauguste@users.noreply.github.com>
2021-10-05 15:07:33 +0200

    1.3 tri par ordre croissant

--- clp_tp1/tris.cpp
+++ clp_tp1/tris.cpp
@@ -16,7 +16,7 @@
 #include <vector>
 
 
-void print_tab(const std::vector< int > tab) {
+void print_tab(const std::vector< int > &tab) {
     std::cout << "[ ";
     for (int i=0; i<tab.size(); i++) {
         std::cout << tab[i] << " ";
@@ -27,6 +27,20 @@ void print_tab(const std::vector< int > tab) {
 void random_tab(std::vector< int > &tab) {
     for (int i=0; i<tab.size(); i++) {
         tab[i] = rand() % 21 - 10;
+    }
+}
+
+void sort_tab_1(std::vector< int > &tab) {
+    for (int i=0; i<tab.size();i++) {
+        int j_min = i;
+        int min = tab[i];
+        for (int j=i; j<tab.size(); j++) {
+            if (tab[j]<min) {
+                min = tab[j];
+                j_min = j;
+            }
+        }
+        std::swap(tab[i], tab[j_min]);
     }
 }
 
@@ -45,12 +59,25 @@ void test_12() {
     std::cout << std::endl;
 }
 
+void test_13() {
+    std::cout << std::endl << "*** test_13 ***" << std::endl;
+    std::vector< int > tab (10);
+    random_tab(tab);
+    std::cout << std::endl << "Random tab" << std::endl;
+    print_tab(tab);
+    sort_tab_1(tab);
+    std::cout << std::endl << "Sorted tab" << std::endl;
+    print_tab(tab);
+    std::cout << std::endl;
+}
+
 int main()
 {
     std::srand( std::time( nullptr ));
 
     //test_11();
-    test_12();
+    //test_12();
+    test_13();
 
     return 0;
 }
